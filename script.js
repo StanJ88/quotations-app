7a4249af6ac36e863100c7227f93214d38d1c544
StanJ88 <stanjamme@live.fr>
2025-07-04 12:44:07 +0200

    Etape 5

--- script.js
+++ script.js
@@ -3,12 +3,32 @@ const form = document.getElementById('formulaire')
 
 
 
+
 form.addEventListener('submit', function (event) {
     event.preventDefault()
 
     let author = form.elements['auteur'].value
     let text = form.elements['citation'].value
-    console.log(text, author)
+    addQuote(text, author)
 
 })
 
+
+function addQuote(quote, author) {
+    let quoteText = document.createElement('p')
+    quoteText.classList.add('text')
+    quoteText.innerText = quote
+    let authors = document.createElement('p')
+    authors.classList.add('author')
+    authors.innerText = author
+    const quoteDiv = document.createElement('div')
+    quoteDiv.classList.add('quote')
+
+quoteDiv.appendChild(quoteText)
+quoteDiv.appendChild(authors)
+
+const quoteList = document.getElementById('quote-list')
+quoteList.appendChild(quoteText)
+quoteList.appendChild(authors)
+
+}
